c26db9ad89e02c572f27f0ca01b9082faf0a07d6
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-11-09 07:23:15 +0100

    Changed to https cloning which does not need a github account

--- clone-submodules.sh
+++ clone-submodules.sh
@@ -6,9 +6,11 @@ echo "Clone STARTED:: " >./clone-submodules.log
 echo `date` >>./clone-submodules.log
 for pkg in `cat $PO_LIST`; do
         echo "$pkg" >>./clone-submodules.log
-	git submodule add git@github.com:project-open/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
+#	git submodule add git@github.com:project-open/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
+	git submodule add https://github.com/project-open/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
 done
 for pkg in `cat $COGN_LIST`; do
         echo "$pkg" >>./clone-submodules.log
-	git submodule add git@github.com:cognovis/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
+#	git submodule add git@github.com:cognovis/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
+	git submodule add https://github.com/cognovis/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
 done
