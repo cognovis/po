3208b502f60bfa675e4ab2326c6b93dd32229643
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-11-10 08:40:24 +0100

    Changed to support the upgrade process

--- clone-submodules.sh
+++ clone-submodules.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-COGN_LIST=./cognovis-packages.txt
-PO_LIST=./projop-packages.txt
+COGN_LIST=./cognovis-packages-full.txt
 echo "Clone STARTED:: " >./clone-submodules.log
 echo `date` >>./clone-submodules.log
-for pkg in `cat $PO_LIST`; do
-        echo "$pkg" >>./clone-submodules.log
-#	git submodule add git@github.com:project-open/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
-	git submodule add https://github.com/project-open/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
-done
 for pkg in `cat $COGN_LIST`; do
         echo "$pkg" >>./clone-submodules.log
-#	git submodule add git@github.com:cognovis/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
-	git submodule add https://github.com/cognovis/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
+	git submodule add git@github.com:cognovis/${pkg}.git packages/$pkg >>./clone-submodules.log 2>&1
+	cd packages/$pkg
+	git remote add upstream git@github.com:project-open/${pkg}.git
+	cd ../..
 done
