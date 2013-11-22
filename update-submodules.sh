a35d4e3d142c8c44469fecf719577f16caa539dd
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-11-22 14:31:46 +0100

    New updates

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-COGN_LIST=~/cognovis-packages.txt
-PO_LIST=~/projop-packages.txt
+COGN_LIST=../cognovis-packages-full.txt
 echo "Update STARTED:: " >./update-submodules.log
 echo `date` >>./update-submodules.log
 cd packages
-for pkg in `cat $PO_LIST`; do
-	cd $pkg
-	git pull origin master  >>~/update-submodules.log 2>&1
-        cd ..
-done
 for pkg in `cat $COGN_LIST`; do
 	cd $pkg
-	git pull origin master  >>~/update-submodules.log 2>&1
+	git pull origin master  >>../../update-submodules.log 2>&1
+	git pull upstream master  >>../../update-submodules.log 2>&1
+	git push origin master  >>../../update-submodules.log 2>&1
         cd ..
 done
