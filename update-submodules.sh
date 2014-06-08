80482e7462bd79b8afcd4358a5fa98269f27e941
Malte Sussdorff <malte.sussdorff@cognovis.de>
2014-06-08 09:04:21 +0200

    Fixed update-submodules to work with 4-1-0-0-0 tag

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-COGN_LIST=~/cognovis-packages.txt
-PO_LIST=~/projop-packages.txt
+COGN_LIST=../cognovis-packages.txt
+PO_LIST=../projop-packages.txt
 echo "Update STARTED:: " >./update-submodules.log
 echo `date` >>./update-submodules.log
 cd packages
 for pkg in `cat $PO_LIST`; do
 	cd $pkg
- 	git fetch --tags upstream master  >>~/update-submodules.log 2>&1
-	git merge tags/v4-1-0-0-0 >>~/update-submodules.log 2>&1
+ 	git fetch --tags origin master  >>../../update-submodules.log 2>&1
+	git merge tags/v4-1-0-0-0 >>../../update-submodules.log 2>&1
         cd ..
 done
 for pkg in `cat $COGN_LIST`; do
 	cd $pkg
-	git pull origin master  >>~/update-submodules.log 2>&1
+	git pull origin master  >>../../update-submodules.log 2>&1
         cd ..
 done
