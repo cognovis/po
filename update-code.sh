54481d5c7b40dd2cb3276c91173fd6cf53b5e22f
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-06-17 11:19:50 +0200

    Added new script for updating the code

---
+++ update-code.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Update STARTED:: " >./update-code.log
+echo `date` >>./update-code.log
+git pull origin nts >>./update-code.log 2>&1
+git submodule init >>./update-code.log 2>&1
+git submodule update >>./update-code.log 2>&1
