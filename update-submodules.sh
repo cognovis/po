3cec18b8706258c37902973e5d697ca70d8bdc75
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-06-17 09:32:37 +0200

    Cleaned up as the repository now contains the submodules

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,11 +2,9 @@
 
 echo "Update STARTED:: " >./update-submodules.log
 echo `date` >>./update-submodules.log
-for pkg in `ls packages`; do
-	cd packages/$pkg
+cd packages
+for pkg in `ls -d */`; do
+	cd $pkg
 	git pull origin master  >>../../update-submodules.log 2>&1
-	git fetch --tags upstream master  >>../../update-submodules.log 2>&1
-	git merge tags/v4-1-0-0-0 >>../../update-submodules.log 2>&1
-	git push origin master  >>../../update-submodules.log 2>&1
-        cd ../..
+        cd ..
 done
