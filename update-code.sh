1373564aed9d215b12ea910e205c198883f5743a
Malte Sussdorff (flyhh) <malte.sussdorff@cognovis.de>
2014-06-26 08:39:50 +0200

    Made sure to use flyhh instead of kolibri

--- update-code.sh
+++ update-code.sh
@@ -2,6 +2,6 @@
 
 echo "Update STARTED:: " >./update-code.log
 echo `date` >>./update-code.log
-git pull origin kolibri >>./update-code.log 2>&1
+git pull origin flyhh >>./update-code.log 2>&1
 git submodule init >>./update-code.log 2>&1
 git submodule update >>./update-code.log 2>&1
