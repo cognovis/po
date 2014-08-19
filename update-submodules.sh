f9cf5e9acb3637f361c5aa3fc31410f583b2984a
Malte Sussdorff (Kolibri) <malte.sussdorff@cognovis.de>
2014-08-19 17:26:27 +0200

    Fixed a couple of issues from kolibri

--- update-submodules.sh
+++ update-submodules.sh
@@ -3,8 +3,9 @@
 echo "Update STARTED:: " >./update-submodules.log
 echo `date` >>./update-submodules.log
 cd packages
-for pkg in `ls -d */`; do
+for pkg in `ls -d */`; do 
 	cd $pkg
+        git checkout master
 	git pull origin master  >>../../update-submodules.log 2>&1
         cd ..
 done
