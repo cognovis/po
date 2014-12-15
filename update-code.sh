6ae5975aa9affe42b6fe17a211ce1f4dd2ec6287
Malte Sussdorff (NTS) <malte.sussdorff@cognovis.de>
2014-12-15 16:15:37 +0100

    Fixed update-code

--- update-code.sh
+++ update-code.sh
@@ -7,5 +7,5 @@ echo `date` >>./update-code.log
 git pull origin nts >>./update-code.log 2>&1
 git submodule init >>./update-code.log 2>&1
 git submodule update >>./update-code.log 2>&1
-echo "Update ENDED:: " >./update-code.log
+echo "Update ENDED:: " >>./update-code.log
 echo `date` >>./update-code.log
